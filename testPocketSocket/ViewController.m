0af318412907f9c4bbf96233a5d1e02a07825353
jin <158555144@qq.com>
2016-01-07 23:53:51 +0800

    更新文件
    更新文件

--- testPocketSocket/ViewController.m
+++ testPocketSocket/ViewController.m
@@ -8,16 +8,19 @@
 
 #import "ViewController.h"
 #import "PSWebSocket.h"
+#import "PSWebSocketServer.h"
 
-@interface ViewController ()<PSWebSocketDelegate>
+@interface ViewController ()<PSWebSocketDelegate,PSWebSocketServerDelegate>
 
 @property (nonatomic, strong) PSWebSocket *socket;
+@property (nonatomic, strong) PSWebSocketServer* server;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    http://127.0.0.1:9013/
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -40,6 +43,17 @@
 
 }
 
+- (IBAction)buttonAction2:(id)sender {
+    
+    _server = [PSWebSocketServer serverWithHost:@"127.0.0.1" port:9013];
+    
+    _server.delegate = self;
+    
+    [_server start];
+}
+
+
+
 #pragma mark - PSWebSocketDelegate
 
 - (void)webSocketDidOpen:(PSWebSocket *)webSocket {
@@ -56,28 +70,28 @@
     NSLog(@"The websocket closed with code: %@, reason: %@, wasClean: %@", @(code), reason, (wasClean) ? @"YES" : @"NO");
 }
 
-
-//- (void)serverDidStart:(PSWebSocketServer *)server {
-//    NSLog(@"Server did start…");
-//}
-//- (void)serverDidStop:(PSWebSocketServer *)server {
-//    NSLog(@"Server did stop…");
-//}
-//- (BOOL)server:(PSWebSocketServer *)server acceptWebSocketWithRequest:(NSURLRequest *)request {
-//    NSLog(@"Server should accept request: %@", request);
-//    return YES;
-//}
-//- (void)server:(PSWebSocketServer *)server webSocket:(PSWebSocket *)webSocket didReceiveMessage:(id)message {
-//    NSLog(@"Server websocket did receive message: %@", message);
-//}
-//- (void)server:(PSWebSocketServer *)server webSocketDidOpen:(PSWebSocket *)webSocket {
-//    NSLog(@"Server websocket did open");
-//}
-//- (void)server:(PSWebSocketServer *)server webSocket:(PSWebSocket *)webSocket didCloseWithCode:(NSInteger)code reason:(NSString *)reason wasClean:(BOOL)wasClean {
-//    NSLog(@"Server websocket did close with code: %@, reason: %@, wasClean: %@", @(code), reason, @(wasClean));
-//}
-//- (void)server:(PSWebSocketServer *)server webSocket:(PSWebSocket *)webSocket didFailWithError:(NSError *)error {
-//    NSLog(@"Server websocket did fail with error: %@", error);
-//}
+#pragma mark - PSWebSocketServerDelegate
+- (void)serverDidStart:(PSWebSocketServer *)server {
+    NSLog(@"Server did start…");
+}
+- (void)serverDidStop:(PSWebSocketServer *)server {
+    NSLog(@"Server did stop…");
+}
+- (BOOL)server:(PSWebSocketServer *)server acceptWebSocketWithRequest:(NSURLRequest *)request {
+    NSLog(@"Server should accept request: %@", request);
+    return YES;
+}
+- (void)server:(PSWebSocketServer *)server webSocket:(PSWebSocket *)webSocket didReceiveMessage:(id)message {
+    NSLog(@"Server websocket did receive message: %@", message);
+}
+- (void)server:(PSWebSocketServer *)server webSocketDidOpen:(PSWebSocket *)webSocket {
+    NSLog(@"Server websocket did open");
+}
+- (void)server:(PSWebSocketServer *)server webSocket:(PSWebSocket *)webSocket didCloseWithCode:(NSInteger)code reason:(NSString *)reason wasClean:(BOOL)wasClean {
+    NSLog(@"Server websocket did close with code: %@, reason: %@, wasClean: %@", @(code), reason, @(wasClean));
+}
+- (void)server:(PSWebSocketServer *)server webSocket:(PSWebSocket *)webSocket didFailWithError:(NSError *)error {
+    NSLog(@"Server websocket did fail with error: %@", error);
+}
 
 @end
